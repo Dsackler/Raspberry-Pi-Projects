7c4bc3e3ece829e2bc39b792bc2eb63a5eec6e62
Dsackler <dsackler@lion.lmu.edu>
2021-05-19 16:44:24 -0700

    Added btn integration

--- Active-Aero.py
+++ Active-Aero.py
@@ -8,25 +8,49 @@ servo1 = GPIO.PWM(11, 50)
 btnPin = 36
 
 
-GPIO.setup(btnPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.add_event_detect(btnPin, GPIO.FALLING, callback = servomov)
+
+def setup():
+    GPIO.setup(btnPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    #GPIO.add_event_detect(btnPin, GPIO.FALLING, callback = servomov)
+    servo1.start(0)
+    servo1.ChangeDutyCycle(7)
+    time.sleep(0.3)
+    servo1.ChangeDutyCycle(0)
 
 
 
-servo1.start(0)
+
+
+def servomov():
+    flap = True
+    while True:
+        input_state = GPIO.input(btnPin)
+        
+        if input_state == False and flap == True:
+            flap = False
+            print("btn")
+            servo1.ChangeDutyCycle(2)
+            time.sleep(0.3)
+            servo1.ChangeDutyCycle(0)
+    
+        elif input_state == False and flap == False:
+            flap = True
+            print("btn 2")
+            servo1.ChangeDutyCycle(7)
+            time.sleep(0.3)
+            servo1.ChangeDutyCycle(0)
+            
+        
+def destroy():
+    servo1.stop()
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    setup()
+    try:
+        servomov()
+    except KeyboardInterrupt:
+        destroy()
 
 
 
-
-
-def servomov(ev = None):
-    servo1.ChangeDutyCycle(7) #go to 90 degrees
-    time.sleep(0.5)
-    servo1.ChangeDutyCycle(0) #turn off to get rid of jitter
-
-    servo1.ChangeDutyCycle(2)
-    time.sleep(0.5)
-
-
-servo1.stop()
-GPIO.cleanup()
